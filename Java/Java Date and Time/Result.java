dd06fe9e09b64fe6485573ce82a2bb1bd896f87a
JAVED ALI <30997178+MJavedAli@users.noreply.github.com>
2019-08-11 20:24:49 +0530

    Updated Code

---
+++ Java/Java Date and Time/Result.java
@@ -0,0 +1,50 @@
+//Problem: https://www.hackerrank.com/challenges/java-date-and-time
+//Java 8
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.time.LocalDate;
+
+
+class Result {
+
+   
+    public static String findDay(int month, int day, int year) {
+    return LocalDate.of(year, month, day).getDayOfWeek().name();
+
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
